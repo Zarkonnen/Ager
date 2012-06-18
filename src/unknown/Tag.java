bedafe029b065aa3b1fc913a9d75f2b00287959e
David Stark <david.stark@zarkonnen.com>
2012-06-18 21:32:20 +0200

    Externalised dependencies.

--- src/unknown/Tag.java
+++ src/unknown/Tag.java
@@ -5,8 +5,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
 /**
  * NBT IO class
